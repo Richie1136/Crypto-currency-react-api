790d75ec926a565cf32e116e13498a1eefb850b9
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-05-17 11:04:00 -0400

    Added a crypto image to put inside the navBar

--- cryptocurrency/src/components/navbar/NavBar.js
+++ cryptocurrency/src/components/navbar/NavBar.js
@@ -1,17 +1,17 @@
 import { Button, Menu, Typography, Avatar } from 'antd'
 import { Link } from 'react-router-dom'
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons'
+import icon from '../../images/cryptocurrency.png'
 
 const NavBar = () => {
   return (
     <div className='nav-container'>
       <div className='logo-container'>
-        <Avatar />
+        <Avatar src={icon} size="large" />
         <Typography.Title className='logo' level={2}>
           <Link to="/">Crypto</Link>
         </Typography.Title>
         <Button className='menu-control-container'>
-
         </Button>
       </div>
     </div>
