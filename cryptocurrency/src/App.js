db2f667117d91cf7f518ccbd4226db993f083977
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-05-17 10:52:51 -0400

    Building out the NavBar component and importing it inside of the App.js file

--- cryptocurrency/src/App.js
+++ cryptocurrency/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { Routes, Route, Link } from 'react-router-dom'
 import { Layout, Typography, Space } from 'antd'
+import NavBar from './components/navbar/NavBar';
 
 function App() {
   return (
     <div className="app">
       <div className='navbar'>
+        <NavBar />
       </div>
       <div className='main'>
       </div>
--- cryptocurrency/src/components/navbar/NavBar.js
+++ cryptocurrency/src/components/navbar/NavBar.js
@@ -1,8 +1,20 @@
 import { Button, Menu, Typography, Avatar } from 'antd'
+import { Link } from 'react-router-dom'
+import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className='nav-container'>
+      <div className='logo-container'>
+        <Avatar />
+        <Typography.Title className='logo' level={2}>
+          <Link to="/">Crypto</Link>
+        </Typography.Title>
+        <Button className='menu-control-container'>
+
+        </Button>
+      </div>
+    </div>
   )
 }
 
