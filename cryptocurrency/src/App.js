94e11307f9c9877e691576bc39ce800ec5fc8226
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-05-17 10:44:21 -0400

    Imported packages from react-router-dom and antd and then started to outline my application with setting up divs

--- cryptocurrency/src/App.js
+++ cryptocurrency/src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+import { Routes, Route, Link } from 'react-router-dom'
+import { Layout, Typography, Space } from 'antd'
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <div className='navbar'>
+      </div>
+      <div className='main'>
+      </div>
+      <div className='footer'>
+      </div>
       <h2>Crypto App</h2>
     </div>
   );
