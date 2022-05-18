5cf2647f53baee70eeb496a160c4532b225ccf5e
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-05-18 10:32:07 -0400

    Created 3 different links inside of my navBar component

--- cryptocurrency/src/components/navbar/NavBar.js
+++ cryptocurrency/src/components/navbar/NavBar.js
@@ -11,9 +11,23 @@ const NavBar = () => {
         <Typography.Title className='logo' level={2}>
           <Link to="/">Crypto</Link>
         </Typography.Title>
-        <Button className='menu-control-container'>
-        </Button>
+        {/* <Button className='menu-control-container'>
+        </Button> */}
       </div>
+      <Menu theme='dark'>
+        <Menu.Item icon={<HomeOutlined />}>
+          <Link to="/"></Link>
+        </Menu.Item>
+        <Menu.Item icon={<FundOutlined />}>
+          <Link to="/cryptos">Cryptocurrencies</Link>
+        </Menu.Item>
+        <Menu.Item icon={<MoneyCollectOutlined />}>
+          <Link to="/exchanges">Exchanges</Link>
+        </Menu.Item>
+        <Menu.Item icon={<BulbOutlined />}>
+          <Link to="/news">News</Link>
+        </Menu.Item>
+      </Menu>
     </div>
   )
 }
