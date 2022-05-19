7c50b55a824205ee7e16cc0f65218b59e8b5a38d
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-05-19 10:35:37 -0400

    Building out the Homepage component with 5 columns within a row with title of total cryptocurrencies, total exchanges, total market cap, total 24h volume and total markets and added dummy values of 5 for all of those rows

--- cryptocurrency/src/components/homepage/Homepage.js
+++ cryptocurrency/src/components/homepage/Homepage.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import millify from 'millify'
+import { Typography, Row, Col, Statistic } from 'antd'
+import { NavLink } from 'react-router-dom'
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <>
+      <Typography.Title className='heading' level={2}>
+        Global Crytpo Stats
+      </Typography.Title>
+      <Row>
+        <Col span={12}><Statistic title='Total Cryptocurrencies' value="5" /></Col>
+        <Col span={12}><Statistic title='Total Exchanges' value="5" /></Col>
+        <Col span={12}><Statistic title='Total Market Cap' value="5" /></Col>
+        <Col span={12}><Statistic title='Total 24h Volume' value="5" /></Col>
+        <Col span={12}><Statistic title='Total Markets' value="5" /></Col>
+      </Row>
+    </>
   )
 }
 
