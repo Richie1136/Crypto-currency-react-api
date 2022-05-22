d9661e9b0e8ecbd3abcf85a0379ec3991abd28b4
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-05-22 10:32:35 -0400

    Mapped over all of the cryptos and am displaying the crypto title, the crypto image, the crypto price, the crypto market Cap and the percent change

--- cryptocurrency/src/components/cryptocurrencies/Cryptocurrencies.js
+++ cryptocurrency/src/components/cryptocurrencies/Cryptocurrencies.js
@@ -4,12 +4,33 @@ import { Card, Row, Col, Input } from "antd"
 import { useGetCryptosQuery } from "../../api"
 import { useState } from "react"
 
+// Millify converts really long numbers to human readable numbers
+
 const Cryptocurrencies = () => {
   const { data: cryptosList, isFetching } = useGetCryptosQuery()
   const [cryptos, setCryptos] = useState(cryptosList?.data.coins)
 
+
   return (
-    <div>Cryptos</div>
+    <>
+      <Row className="crypto-card-container" gutter={[32, 32]}>
+        {cryptos.map((crypto) => (
+          <Col key={crypto.id} className="crypto-card" xs={24} sm={12} lg={6}>
+            <NavLink to={`/crypto/${crypto.id}`}>
+              <Card
+                title={`${crypto.rank}.${crypto.name}`}
+                extra={<img className="crypto-image" src={crypto.iconUrl} />}
+                hoverable
+              >
+                <p>Price: ${millify(crypto.price)}</p>
+                <p>Market Cap: ${millify(crypto.marketCap)}</p>
+                <p>Daily Change: ${millify(crypto.change)}%</p>
+              </Card>
+            </NavLink>
+          </Col>
+        ))}
+      </Row>
+    </>
   )
 }
 
