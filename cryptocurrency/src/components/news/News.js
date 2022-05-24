6b2e196adc814b5f940a6b3706040ce304c1b389
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-05-24 11:06:50 -0400

    Building the News page Created a crypto news api, added it to the News component and also added it to my store

--- cryptocurrency/src/components/news/News.js
+++ cryptocurrency/src/components/news/News.js
@@ -1,6 +1,40 @@
+import { Select, Typography, Row, Col, Avatar, Card } from "antd"
+import moment from "moment"
+import { useGetCryptosNewsQuery } from "../../api/cryptoNews"
+import Loading from "../loading/Loading"
+
+
 const News = ({ simplified }) => {
+
+  const { data: cryptoNews } = useGetCryptosNewsQuery({ newsCategory: 'Cryptocurrency', count: simplified ? 6 : 12 })
+
+  console.log(cryptoNews)
+
+  if (!cryptoNews?.value) return <Loading />
+
+
   return (
-    <div>This is the news</div>
+    <>
+      <Row gutter={[24, 24]}>
+        {cryptoNews?.value.map((news, i) => (
+          <Col key={i} xs={24} sm={12} lg={6}>
+            <Card
+              className="news-card"
+              hoverable
+            >
+              <a href={news.url} target="_blank" rel="noreferrer">
+                <div className="news-image-container">
+                  <Typography.Title className="news-title" level={4}>
+                    {news.name}
+                  </Typography.Title>
+                  <img src={news?.image?.thumbnail.contentUrl} />
+                </div>
+              </a>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </>
   )
 }
 
--- cryptocurrency/src/store/store.js
+++ cryptocurrency/src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { cryptoApi } from '../api/index'
+import { cryptoNewsApi } from "../api/cryptoNews";
 
 export default configureStore({
   reducer: {
     [cryptoApi.reducerPath]: cryptoApi.reducer,
+    [cryptoNewsApi.reducerPath]: cryptoNewsApi.reducer
   },
 })
