d64a18de2d9f7882010271ef2041d6cc05426e56
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-05-29 10:41:13 -0400

    Added a line chart component and imported packages from antd and react chart js

---
+++ cryptocurrency/src/components/linechart/LineChart.js
@@ -0,0 +1,10 @@
+import { Line } from "react-chartjs-2"
+import { Row, Col, Typography } from "antd"
+
+const LineChart = () => {
+  return (
+    <div>LineChart</div>
+  )
+}
+
+export default LineChart
