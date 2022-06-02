a6e02f428854cb25c5fb245278c7a3b446a9cc83
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-06-02 10:48:09 -0400

    Imported packages from antd and created a exchange route and added it to my Exchanges component

--- cryptocurrency/src/api/index.js
+++ cryptocurrency/src/api/index.js
@@ -21,9 +21,11 @@ export const cryptoApi = createApi({
     }),
     getCryptoHistory: builder.query({
       query: ({ coinId, timeperiod }) => createRequest(`coin/${coinId}/history?timeperiod=${timeperiod}`),
-
+    }),
+    getCryptoExchanges: builder.query({
+      query: () => createRequest('/exchanges'),
     })
   })
 });
 
-export const { useGetCryptosQuery, useGetCryptoDetailsQuery, useGetCryptoHistoryQuery } = cryptoApi;
+export const { useGetCryptosQuery, useGetCryptoDetailsQuery, useGetCryptoHistoryQuery, useGetCryptoExchangesQuery } = cryptoApi;
--- cryptocurrency/src/components/exchanges/Exchanges.js
+++ cryptocurrency/src/components/exchanges/Exchanges.js
@@ -1,5 +1,11 @@
+import { Col, Row, Avatar, Typography, Collapse } from "antd"
+import HTMLReactParser from "html-react-parser"
+import { useGetCryptoExchangesQuery } from "../../api"
+import Loading from "../loading/Loading"
+
 
 const Exchanges = () => {
+  const { data, isFetching } = useGetCryptoExchangesQuery()
   return (
     <div>Exchanges</div>
   )
